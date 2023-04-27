0c0d48b2dfac15831e5d233cd1e9d9f031a5afb7
alexandertao <tao00029@umn.edu>
2023-04-26 20:10:49 -0500

    update nenci script

--- nenci_inProcess.py
+++ nenci_inProcess.py
@@ -7,34 +7,26 @@ from colabfit.tools.configuration import AtomicConfiguration
 client = MongoDatabase('new_data_test_alexander', configuration_type=AtomicConfiguration, nprocs=4, drop_database=True)
 
 
+def reader(file_path):
+    file_name=file_path.stem
+    atom=ase.io.read(file_path)
+    atom.info['name'] = file_name
+    yield atom
+
 # Loads data, specify reader function if not "usual" file format
 configurations = load_data(
-    file_path='/large_data/new_raw_datasets_2.0/nenci2021/nenci2021/xyzfiles/reformat/141_Cl-pyridine_A_z_-30_1.10_reformat.xyz',
-    file_format='xyz',
-    name_field=None,
+    file_path='/large_data/new_raw_datasets_2.0/nenci2021/nenci2021/xyzfiles/',
+    file_format='folder',
+    name_field='name',
     elements=['H','C', 'N', 'O', 'F', 'S', 'P', 'Cl', 'Br'],
-    default_name='nanci',
-    verbose=True,
-    generator=False
-)
-'''
-configurations += load_data(
-    file_path='/colabfit/data/data/gubaev/AlNiTi/train_2nd_stage.cfg',
-    file_format='cfg',
-    name_field=None,
-    elements=['Al', 'Ni', 'Ti'],
-    default_name='train_2nd_stage',
+    reader=reader,
+    glob_string='*reformat.xyz'
+    #default_name='nanci',
     verbose=True,
     generator=False
 )
 
-cs_list = set()
-for c in configurations:
-    cs_list.add(*c.info['_name'])
-print(cs_list)
-'''
 # In[ ]:
-
 
 client.insert_property_definition('/home/ubuntu/notebooks/potential-energy.json')
 #client.insert_property_definition('/home/ubuntu/notebooks/atomic-forces.json')
@@ -53,7 +45,7 @@ client.insert_property_definition(free_property_definition)
 
 property_map = {
         'potential-energy': [{
-            'energy':   {'field': 'energy',  'units': 'kcal/mol'},
+            'energy':   {'field': 'CCSD(T)/CBS',  'units': 'kcal/mol'},
             'per-atom': {'field': 'per-atom', 'units': None},
      #For metadata want: software, method (DFT-XC Functional), basis information, more generic parameters
             '_metadata': {
@@ -107,41 +99,26 @@ ids = list(client.insert_data(
 
 all_co_ids, all_pr_ids = list(zip(*ids))
 
-'''
-#matches to data CO "name" field
-cs_regexes = {
-    '.*':
-        'Silica datasets. For DFT computations, the GPAW (in combination with ASE) and VASP codes employing '\
-        'the projector augmented-wave method were used. Early versions of the GAP were based '\
-        'on reference data computed using the PBEsol functional. For GPAW, an energy cut-off '\
-        'of 700 eV and a k-spacing of 0.279 Å−1 were used, for VASP, a higher energy cut-off '\
-        'of 900 eV and a denser k-spacing of 0.23 Å−1 were used.',
-        }
-cs_names=['all']
-for i in cs_list:
-    cs_regexes[i]='Configurations with the %s structure.' %i
-    cs_names.append(i)
 
-#print (cs_regexes)
-'''
+cs_info = [
 
+    {"name":"Water",
+    "description": "Configurations with water structure"},
+
+    {"name": "MeOH",
+    "description": "Configurations with MeOH structure"},
+]
 cs_ids = []
 
-'''
-for i, (regex, desc) in enumerate(cs_regexes.items()):
-    co_ids = client.get_data(
-        'configurations',
-        fields='hash',
-        query={'hash': {'$in': all_co_ids}, 'names': {'$regex': regex}},
-        ravel=True
-    ).tolist()
-
-    print(f'Configuration set {i}', f'({regex}):'.rjust(22), f'{len(co_ids)}'.rjust(7))
-
-    cs_id = client.insert_configuration_set(co_ids, description=desc,name=cs_names[i])
+for i in cs_info:
+    cs_id = client.query_and_insert_configuration_set(
+        co_hashes=all_co_ids,
+        query={'names':{'$regex':i['name']+'_*'}},
+        name=i['name'],
+        description=i['description']
+    )
 
     cs_ids.append(cs_id)
-'''
 
 ds_id = client.insert_dataset(
     cs_ids=cs_ids,
