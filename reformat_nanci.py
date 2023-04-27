34b2e1b09bdedf0c4ea5a649781138df7685cba1
alexandertao <tao00029@umn.edu>
2023-04-26 19:26:47 -0500

    reader function to reformat multiple datasets

--- reformat_nanci.py
+++ reformat_nanci.py
@@ -1,9 +1,11 @@
 import sys
 def reformat(file_name):
     do=True
-    a = file_address.split['.']
-    b = a[0] + '_reformat.' + a[1]
+    a = file_address.split('.')
+    #print(a)
+    b = a[0] + '.' + a[1] + '_reformat.' + a[2]
     nf=open(b,'w')
+    #nf=open(str(file_address+'_reformat'),'w')
     with open(file_address,'r') as f:
         while do:
                 try:
@@ -30,11 +32,9 @@ def reformat(file_name):
                 except:
                     do=False
 
-
 import os
 current_address = os.path.dirname(os.path.abspath(__file__))
 file_list = os.listdir(current_address)
 for file_address in file_list:
-    reformat(file_list)
-
-
+    #a = file_address.split['.']
+    #print (a)
