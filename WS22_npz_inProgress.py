d4cd2144dc8fc0cb84cbb72fe7537dc2f2ed66ae
alexandertao <tao00029@umn.edu>
2023-04-26 20:35:13 -0500

    updates to ws22 script

--- WS22_npz_inProgress.py
+++ WS22_npz_inProgress.py
@@ -15,23 +15,25 @@ client = MongoDatabase('new_data_test_alexander', configuration_type=AtomicConfi
 #multiple properties
 # In[ ]:
 #solvated_protein-check out README in data's directory
-def reader_rmd(p):
+def reader_ws22(p):
     atoms=[]
     a=np.load(p)
     #na=a['N']
-    z=a['nuclear_charges']
-    e=a['energies']
-    r=a['coords']
-    f=a['forces']
-    #d=a['D']
+    z=a['Z']
+    e=a['E']
+    r=a['R']
+    f=a['F']
+    hl=a['HL']
+    d=a['DP']
     #q=a['nuclear_charges']
-    for i in tqdm(range(len(na))):
+    for i in tqdm(range(len(na))):  #need to change it
         #n=na[i]
         atom=Atoms(numbers=z[i,:n],positions=r[i,:n,:])
         #atom.info['energy']=e[i]
         atom.info['energy']=float(e[i])
         atom.arrays['forces']=f[i]
         atom.info['dipole_moment']=d[i]
+        atom.info['homolumo']=hl[i]
         #atom.info['charge']=float(q[i])
         #print(atom.info['charge'])
         atoms.append(atom)
@@ -45,8 +47,8 @@ configurations = load_data(
     name_field=None,
     elements=['C','N','O','H'],
     default_name='ws22_acrolein',
-    reader=reader_rmd,
-    glob_string='ws22_acrolein.npz',
+    reader=reader_ws22,
+    glob_string='ws22',
     verbose=True,
     generator=False
 )
@@ -141,6 +143,7 @@ ids = list(client.insert_data(
 #all_co_ids, all_pr_ids = list(zip(*ids))
 all_cos, all_dos = list(zip(*ids))
 
+'''
 cs_info = [
 
     {"name":"malonaldehyde",
@@ -186,7 +189,7 @@ for i in cs_info:
     )
 
     cs_ids.append(cs_id)
-
+'''
 
 
 # In[ ]:
@@ -195,27 +198,19 @@ for i in cs_info:
 ds_id = client.insert_dataset(
     #cs_ids=cs_ids,
     do_hashes=all_dos,
-    name='RMD17',
+    name='WS22',
     authors=['Pinheiro Jr', 'M., Zhang', 'S., Dral', 'P. O.','Barbatti, M.'],
     links=[
         'https://www.nature.com/articles/s41597-023-01998-3#code-availability',
         'https://zenodo.org/record/7032334#.ZEDJes7MJEY',
     ],
-    description =  'For each of the 10 molecules in the MD17 dataset (aspirin, benzene, ethanol, '\
-    'salicylic acid, malonaldehyde, toluene, naphthalene, uracil, paracetamol, and '\
-    'azobenzene), 100,000 structures were randomly selected from the available MD '\
-    'trajectory data. For each of these structures, a single-point force andenergy '\
-    'evaluation was carried out at the DFT level. All calculations were performed in '\
-    'ORCA 4.0.1, using the PBE functional and the def2-SVP basis set with the '\
-    'resolution-of-identity (RI) approximation for the Coulomb integrals.',
-'''
-The WS22 database combines Wigner sampling with geometry interpolation to generate 1.18 million molecular 
-geometries equally distributed into 10 independent datasets of flexible organic molecules with varying
-sizes and chemical complexity.
-In addition to the potential energy and forces required to construct potential energy surfaces, the WS22 
-database provides several other quantum chemical properties, all obtained via single-point calculations 
-for each molecular geometry. All quantum chemical calculations were performed with the Gaussian09 program.
-'''
+    description =  'The WS22 database combines Wigner sampling with geometry interpolation to generate 1.18 '\
+    'million molecular geometries equally distributed into 10 independent datasets of flexible '\
+    'organic molecules with varying sizes and chemical complexity. '\
+    'In addition to the potential energy and forces required to construct potential energy surfaces, the WS22 '\
+    'database provides several other quantum chemical properties, all obtained via single-point calculations '\
+    'for each molecular geometry. All quantum chemical calculations were performed with the Gaussian09 program.',
+
     resync=True,
     verbose=True,
 
